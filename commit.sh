d48fb7ebf3fbf1c75210aca2734b931a2099acd3
ngoantran12 <ngoantran19112@gmail.com>
2025-04-04 20:27:01 +0700

    Better user authentication 🔐

---
+++ commit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Số commit muốn tạo
+NUM_COMMITS=200
+
+# Lặp tạo commit với ngày tăng dần
+for ((i=NUM_COMMITS; i>=1; i--)); do
+    # Tính ngày (bắt đầu từ 120 ngày trước, tiến về hiện tại)
+    DATE=$(date -d "$i days ago" --iso-8601=seconds)
+
+    # Ghi nội dung vào file (đảm bảo file tồn tại)
+    echo "daily data  $i" > commit.txt
+
+    # Thêm vào Git
+    git add commit.txt
+
+    # Commit với ngày cụ thể
+    GIT_COMMITTER_DATE="$DATE" GIT_AUTHOR_DATE="$DATE" git commit -m "Commit $i on $DATE"
+done
+
+# Đẩy toàn bộ commit lên GitHub
+git push origin main
+
